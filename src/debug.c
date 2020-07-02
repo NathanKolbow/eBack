a94739bfe3ddedeb34cace0032ede4fbad0fda47
root <root@PepPop.medmicro.wisc.edu>
2020-07-01 19:38:54 -0500

    Added fflush() to debug messages

--- src/debug.c
+++ src/debug.c
@@ -7,7 +7,12 @@ int DB_LEVEL = DB_DEBUG;
 
 void d_log(int level, char *msg) {
     if(level <= DB_LEVEL) {
-        if(level == DB_DEBUG || level == DB_EVERYTHING) printf("%s", msg);
-        else fprintf(stderr, "%s", msg);
+        if(level == DB_DEBUG || level == DB_EVERYTHING) {
+		printf("%s", msg);
+		fflush(stdin);
+	} else {
+		fprintf(stderr, "%s", msg);
+		fflush(stderr);
+	}
     }
 }
